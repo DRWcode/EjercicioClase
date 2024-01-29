2d7001fdd6ac6599281fb8c9f24e79d4e37cf891
slowbrox <raudalesdarwin@gmail.com>
2024-01-29 16:02:28 -0600

    primera prueba

--- obj/Debug/net8.0/EjercicioClase.AssemblyInfo.cs
+++ obj/Debug/net8.0/EjercicioClase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioClase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc7444a6f26bd6bde2f088384eadd8fe392ea1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioClase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioClase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
